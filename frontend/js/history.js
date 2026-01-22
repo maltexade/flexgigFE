9e3b163bd94097a8f6ef03204ef6230fb2d133ac
Maltexade <mathewaderibigbe1@gmail.com>
2026-01-22 12:34:31 +0100

    new update

--- frontend/js/history.js
+++ frontend/js/history.js
@@ -1698,7 +1698,7 @@ async function subscribeToTransactions(force = false) {
     if (state.open) {
       applyTransformsAndRender();
     }
-    renderDashboardRecent();
+    
   } else if (payload.eventType === 'INSERT') {
     console.log('[Tx Realtime] Adding new tx:', normalized.reference);
     state.items.unshift(normalized);
